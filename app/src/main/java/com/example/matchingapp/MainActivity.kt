163fe3c8408d1a1455c5adb20af98c198e198bb0
hyuni2 <youthgmldbs@gmail.com>
2025-01-23 09:26:03 +0900

    희윤 merge test

--- app/src/main/java/com/example/matchingapp/MainActivity.kt
+++ app/src/main/java/com/example/matchingapp/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.example.matchingapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
-    // 희윤 merge test
+    // 희윤 merge test 2
     // 온유 merge test
 }
