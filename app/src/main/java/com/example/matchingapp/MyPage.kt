e78dc46b2120136337a12c9553db51eec208a1ee
노희민 <wow1222@swu.ac.kr>
2025-01-23 10:55:58 +0900

    .

--- app/src/main/java/com/example/matchingapp/MyPage.kt
+++ app/src/main/java/com/example/matchingapp/MyPage.kt
@@ -1,39 +1,16 @@
 package com.example.matchingapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class `FirstPage(ToLoginAndRegister)` : AppCompatActivity() {
-    lateinit var login : Button
-    lateinit var join : Button
-
+class MyPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.firstpage)
-
-        login = findViewById<Button>(R.id.login)
-        join = findViewById<Button>(R.id.join)
-
-        login.setOnClickListener{
-            var intent = Intent(this, Login::class.java)
-            startActivity(intent)
-            finish()
-
-        }
-
-        join.setOnClickListener{
-            var intent = Intent(this, Register::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-
+        setContentView(R.layout.activity_my_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
