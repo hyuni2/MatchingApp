6ee504be502c28691ec6960de43eb1f3c597fedc
노희민 <wow1222@swu.ac.kr>
2025-01-23 10:44:42 +0900

    로그인 기능 : 입력한 id/pw가 db 정보와 같으면 MyPage.kt 로 이동. 같지 않으면 오류 메시지 출력.

--- app/src/main/java/com/example/matchingapp/Login.kt
+++ app/src/main/java/com/example/matchingapp/Login.kt
@@ -1,25 +1,57 @@
 package com.example.matchingapp
 
+import DBManager
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import org.w3c.dom.Text
 
+
 class Login : AppCompatActivity() {
-    lateinit var id : TextView
-    lateinit var pw : TextView
+    lateinit var id : EditText
+    lateinit var pw : EditText
     lateinit var checkidpw : Button
+    private lateinit var dbManager: DBManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_create_account)
+        setContentView(R.layout.login)
+
+
+        dbManager = DBManager(this)
 
         id = findViewById(R.id.id)
         pw = findViewById(R.id.pw)
+        checkidpw = findViewById(R.id.checkidpw)
+
+
+        checkidpw.setOnClickListener{
+            val userid = id.text.toString().trim()
+            //입력값 받아 문자열로 변환 뒤 공백제거
+            val userpw = pw.text.toString().trim()
+
+            if (dbManager.loginUser(userid, userpw)){
+                //Id, pw db 정보와 일치할 시 MYpage로 이동 .
+                val intent = Intent(this, MyPage::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                //Id, pw db 정보와 불일치할 시 오류메시지 출력. (개발조건)
+                Toast.makeText(this, "잘못된 정보입니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
