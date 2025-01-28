5c52cf7bb72c3bbc071c83d652e226de0ad9dc5e
노희민 <wow1222@swu.ac.kr>
2025-01-28 22:03:15 +0900

    Login 코드 오류 수정

--- app/src/main/java/com/example/matchingapp/Login.kt
+++ app/src/main/java/com/example/matchingapp/Login.kt
@@ -26,7 +26,7 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.login)
 
 
-        dbManager = DBManager(this)
+        dbManager = DBManager(this, "database_name", null, 1)
 
         id = findViewById(R.id.id)
         pw = findViewById(R.id.pw)
@@ -40,9 +40,11 @@ class Login : AppCompatActivity() {
 
             if (dbManager.loginUser(userid, userpw)){
                 //Id, pw db 정보와 일치할 시 MYpage로 이동 .
-                val intent = Intent(this, MyPage::class.java)
-                startActivity(intent)
-                finish()
+                val fragment = FindMentoMenti_fragment() // 프래그먼트 인스턴스 생성
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.fragmentContainer, fragment) // R.id.fragment_container는 프래그먼트를 담을 컨테이너의 ID입니다.
+                transaction.addToBackStack(null) // 백스택에 추가하여 뒤로 가기 버튼을 눌렀을 때 이전 화면으로 돌아갈 수 있게 합니다.
+                transaction.commit()
             } else {
                 //Id, pw db 정보와 불일치할 시 오류메시지 출력. (개발조건)
                 Toast.makeText(this, "잘못된 정보입니다.", Toast.LENGTH_SHORT).show()
