870f7ef60e5d44db15087cb358eb18dfdccd4d36
노희민 <wow1222@swu.ac.kr>
2025-01-29 20:55:48 +0900

    자잘한 오류 수정2(ing)

--- app/src/main/java/com/example/matchingapp/Register.kt
+++ app/src/main/java/com/example/matchingapp/Register.kt
@@ -28,10 +28,10 @@ class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login)
+        setContentView(R.layout.activity_register)
 
 
-        dbManager = DBManager(this)
+        dbManager = DBManager(this, "regitsterUser", null, 1)
 
         id = findViewById(R.id.newid)
         pw = findViewById(R.id.newpw)
@@ -69,7 +69,7 @@ class Register : AppCompatActivity() {
 
 
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Register)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
